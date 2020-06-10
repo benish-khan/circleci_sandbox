d23897691c30b6160e9704aedadfbc7ced7dfba3
Benish Khan <bsarinelli@gmail.com>
2020-06-09 21:12:26 -0700

    Add script

---
+++ bebes_first_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#this line above tells the computer which type of interpreter to use for the script.
+#When saving the script file, it is good practice to place commonly used scripts in the ~/bin/ directory
+#The script files also need to have the “execute” permission to allow them to be run.
+#chmod +x script.sh
+#OSX, this is ~/.bash_profile
+
+echo "Hello World!"
